179ee40750bd98abe48695b2efe3d4181df61206
Maxim Kapralov <makvasm111@gmail.com>
2021-02-23 04:32:21 +0300

    db connection options for heroku postgres

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const DBConf = require('./dbconfig').DBConf
 const { Sequelize, DataTypes } = require('sequelize')
 
 const dbconf = new DBConf()
-const db = new Sequelize(dbconf.url(), { ssl: true })
+const db = new Sequelize(dbconf.url(), {
+  dialect : 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+      ssl: true
+  }
+})
 
 
 const Room = db.define('Room', {
