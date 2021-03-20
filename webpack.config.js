00a1e0c90eca43e654ccfcf7b81fb1b2e5c95359
Maxim Kapralov <makvasm111@gmail.com>
2021-03-20 11:44:29 +0300

    webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,44 @@
-const path = require("path")
+const path = require("path");
 
 module.exports = {
-    mode: "production",
-    entry: {
-        streaming: "./src/streaming/streaming.js",
-        sockets: "./src/videoplayer/sockets.js",
-        Player: "./src/videoplayer/Player.js",
-        Room: "./src/videoplayer/Room.js",
-        init: "./src/videoplayer/init.js",
+    entry: "./src/index.js",
+    mode: "development",
+    output: {
+        filename: "app.js",
+        path: path.resolve(__dirname, 'public'),
     },
-    output : {
-        filename: "[name].js",
-        path: path.join(__dirname, "/public/")
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+        compress: true,
+        port: 9000,
+        watchContentBase: true,
+        progress: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            }
+        ]
     }
-}
+};
